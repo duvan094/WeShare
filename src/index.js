6dfb49cc0cf84b243ad09ad06dea99e17df42016
Jacob <jacobduvander@gmail.com>
2018-10-01 16:08:15 +0200

    Fixed things in stuff

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ var express = require('express'), router = express.Router();
 
 require('./initDB');
 router.use('/accounts', require('./account'));
+router.use('/groups', require('./group'));
+router.use('/group-members', require('./group_member'));
 
 module.exports = router;
