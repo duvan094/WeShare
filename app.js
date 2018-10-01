ddb0dc44c0cf2aa22af66fa7b2abb50f5bccdc05
OliverRomhagen <oliverromhagen@hotmail.com>
2018-10-01 16:10:11 +0200

    deleted comments

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-//require('./src/init');
-//require('./src/account');
-//require('./src/group');
-//require('./src/group_member');
 const express = require('express');
 
 const app = express();
 
 app.use(require('./src/index'));
-
-//app.use('/src', require('./src'));
-//app.use(require('./src'));
 
 app.listen(3000);
