40cce312d82b4c1084d41256b54037fd044b635d
Jacob <jacobduvander@gmail.com>
2018-10-03 15:01:39 +0200

    Added passport

--- src/variables.js
+++ src/variables.js
@@ -3,6 +3,12 @@ const jwt = require('jsonwebtoken');
 const saltRounds = 10;//How many times the password is hashed
 const serverSecret = "jacobjhaskjhdjakhdkahdjakasdoliver"; //
 
+const appId = "2633953939951775";
+const appSecret = "9d3c87c652a23473635ab66bdbd90e25";
+
 exports.jwt = jwt;
+
 exports.saltRounds = saltRounds;
 exports.serverSecret = serverSecret;
+exports.appId = appId;
+exports.appSecret = appSecret;
