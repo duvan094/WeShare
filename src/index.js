c4d88c20446de47e2185e7045efc6484909542ab
Jacob <jacobduvander@gmail.com>
2018-10-04 11:33:54 +0200

    Added token fiile

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ require('./initDB');
 router.use('/accounts', require('./account'));
 router.use('/groups', require('./group'));
 router.use('/group-members', require('./group_member'));
+router.use('/tokens', require('./tokens'));
 
 module.exports = router;
