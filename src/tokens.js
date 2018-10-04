b8df4079fc907c3c418cdd224650a69a3118caea
OliverRomhagen <oliverromhagen@hotmail.com>
2018-10-04 12:03:36 +0200

    fixed jacobs wrongly used variables

--- src/tokens.js
+++ src/tokens.js
@@ -2,10 +2,10 @@
 
 //Used to login
 
-app.post("/", function(request, response){
+app.post("/", function(req, res){
 
-  const username = request.body.username;
-  const password = request.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
   //First check if user exists
   const query = 'Select * FROM Account WHERE username = ?';
@@ -13,9 +13,9 @@ app.post("/", function(request, response){
 
   db.get(query,values,function(error,account){
     if(error){
-      response.status(500).end();
+      res.status(500).end();
     }else if(!account){//no acount found
-      response.status(400).json(["invalid_client"]);
+      res.status(400).json(["invalid_client"]);
     }else{
       //Check entered password compares to the hashed password in the database
       if(bcrypt.compareSync(password, account.hashedPassword)){
@@ -24,13 +24,13 @@ app.post("/", function(request, response){
         const accessToken = jwt.sign({accountId: account.id}, serverSecret);
         const idToken = jwt.sign({sub:account.id, preferred_username:account.username}, serverSecret);
 
-        response.status(200).json({
+        res.status(200).json({
           access_token: accessToken,
           token_type: "Bearer",
           id_token: idToken
         });
       }else{
-        response.status(400).json({error: "invalid_client"});
+        res.status(400).json({error: "invalid_client"});
       }
     }
   });
