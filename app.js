d46d012d6ddafd9815e081820e8a868b66dedb96
OliverRomhagen <oliverromhagen@hotmail.com>
2018-10-12 12:16:08 +0200

    app comments

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express'); //require the npm package 'express'
 
 const app = express();
 
-app.use(require('./src/index')); // 
+app.use(require('./src/index'));
 
 app.listen(3000); //Binds and listens for connections on the specified host and port
