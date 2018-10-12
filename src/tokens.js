5faf400702eb2da548459487eedcf583958b43af
OliverRomhagen <oliverromhagen@hotmail.com>
2018-10-12 11:38:35 +0200

    Tokens commented

--- src/tokens.js
+++ src/tokens.js
@@ -1,64 +1,65 @@
+// require all npm packages and help files
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
 const initDB = require('./initDB');
 const vars = require('./variables');
 
-router = express.Router();
-
 const serverSecret = vars.serverSecret;
 const db = initDB.db;
-
+// Used to export the module, that will be then used in another file.js
+router = express.Router();
+// Use bodyparser to be able to read bodies written in JSON and XML format
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: false}));
 
-
+// Doing a POST request to create an account 
 router.post("/", function(req, res){
 
   const username = req.body.username;
   const password = req.body.password;
 
-  //First check if user exists
   const query = 'Select * FROM Account WHERE username = ?';
   const values = [username];
 
+ //First check if user exists
   db.get(query,values,function(error,account){
     if(error){
       res.status(500).end();
-    }else if(!account){//no acount found
+    }else if(!account){//no account found? Returning error code invalid_client
       res.status(400).json(["invalid_client"]);
     }else{
-      //Check entered password compares to the hashed password in the database
+      //Checks the entered password, compares it to the hashedpassword in the database.
       if(bcrypt.compareSync(password, account.hashedPassword)){
 
         // Create a new token that can be sent to client
         const accessToken = jwt.sign({accountId: account.id}, serverSecret);
         const idToken = jwt.sign({sub:account.id, preferred_username:account.username}, serverSecret);
-
+        //Returning the accessToken for the user to use in the authorizationHeader
         res.status(200).json({
           access_token: accessToken,
           token_type: "Bearer",
           id_token: idToken
         });
       }else{
+        //If password doesn't match the user is unauthorized 
         res.status(401).json(["Unathorized"]);
       }
     }
   });
 });
 
+// A function to check if the user is authorized. 
 function authorizedUser(req,accountId = null){
   const authorizationHeader = req.get("authorization");
 
+  //Stops the function if the authorization code is undefined
   if(authorizationHeader === undefined){
     return false;
   }
-
-  const accessToken = authorizationHeader.substr(7);//used to remove "Bearer" in the beginning of accessToken
-
-
+    //used to remove "Bearer" in the beginning of accessToken
+    const accessToken = authorizationHeader.substr(7);
 
   let tokenAccountId = null;
 
@@ -68,7 +69,7 @@ function authorizedUser(req,accountId = null){
   }catch(error){//if the payload fails it means it is tempered with
     return false;
   }
-
+  
   if(accountId !== null){
     if(tokenAccountId !== accountId){//Check so accountId matches the one saved in the token
       return false;
@@ -81,6 +82,6 @@ function authorizedUser(req,accountId = null){
 
 }
 
-
+//Exports the modules 
 module.exports = router;
 module.exports.authorizedUser = authorizedUser;
